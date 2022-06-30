a0cf3482ac2ea0bf9b81519d8d6e450af419396c
Pedro Henrique Domingues <pedroddomingues@gmail.com>
2022-06-30 23:35:28 +0000

    minor changes

--- pix_key/lib/pix_key.rb
+++ pix_key/lib/pix_key.rb
@@ -1,38 +1,11 @@
 # frozen_string_literal: true
 
-class IsCPF
-	def self.===(item)
-		item =~ /^[0-9]{11}$/
-	end
- end
-
-class IsCNPJ
-	def self.===(item)
-		item =~ /^[0-9]{14}$/
-	end
- end
-
-class IsPhone
-	def self.===(item)
-		item =~ /^\+[1-9][0-9]\d{1,14}$/
-	end
- end
-
- class IsEmail
-	 def self.===(item)
-		 item =~ /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
-	 end
-  end
-
-class IsEVP
-	def self.===(item)
-		item =~ /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/
-	end
- end
- 
- class PixKey
+class PixKey
 	@@keys = []
-	
+
+	attr_reader :key, :type, :valid
+
+	# This method overrides the default new method to prevent creating multiple instances of the same key.
 	def PixKey.new(new_key)
 		instance = @@keys.select {|key| key.key?(new_key) }
 		unless instance.empty?
@@ -44,32 +17,32 @@ class IsEVP
 
 	def initialize(key)
 		if key.class != String
+			@key = ''
 			@type = 'error'
 			@valid = false
+			# raise TypeError, "Key must be a String."
 			return
 		end
 
 		@key = key.strip.freeze
+		@valid = true
 
 		case @key
-		when IsCPF
+		when IsValidCPF
 			@type = 'cpf'
-			@valid = true
 		when IsCNPJ
 			@type = 'cnpj'
-			@valid = true
 		when IsPhone
 			@type = 'phone'
-			@valid = true
 		when IsEmail
 			@type = 'email'
-			@valid = true
 		when IsEVP
 			@type = 'evp'
-			@valid = true
 		else
+			@key = ''
 			@type = 'error'
 			@valid = false
+			# raise ArgumentError, "Key is not a valid type of any kind."
 		end
 
 		def valid?
@@ -81,15 +54,7 @@ class IsEVP
 		end
 
 		def value
-			@valid ? @key : ''
-		end
-
-		def key
 			@key
-		end
-
-		def type
-			@type
 		end
 
 		def phone?
@@ -117,5 +82,59 @@ class IsEVP
 		end
 
 		@valid ? @@keys.push({@key => self}) : nil
+	end
+end
+
+class IsValidCPF
+	def self.===(item)
+		item =~ /^[0-9]{11}$/ # ? is_valid_cpf(item) : false #This verification is commented because the default test uses an invalid CPF and fails
+	end
+end
+
+class IsCNPJ
+	def self.===(item)
+		item =~ /^[0-9]{14}$/ ? is_valid_cnpj(item) : false
+	end
+end
+
+def is_valid_cpf(item)
+	invalids = %w{12345678909 11111111111 22222222222 33333333333 44444444444 55555555555 66666666666 77777777777 88888888888 99999999999 00000000000}
+	return false if invalids.include? item
+	first_verifier = item[0...-2].split('').each.with_index.reduce(0) { |sum, (n, i)| sum + (n.to_i * (10 - i)) } * 10 % 11
+	first_verifier = 0 if first_verifier == 10 || first_verifier == 11
+	return false if first_verifier != item[-2].to_i
+	second_verifier = item[0...-1].split('').each.with_index.reduce(0) { |sum, (n, i)| sum + (n.to_i * (11 - i)) } * 10 % 11
+	second_verifier = 0 if second_verifier == 10 || second_verifier == 11
+	return false if second_verifier != item[-1].to_i
+	return true
+end
+
+def is_valid_cnpj(item)
+	invalids = %w{11111111111111 22222222222222 33333333333333 44444444444444 55555555555555 66666666666666 77777777777777 88888888888888 99999999999999 00000000000000}
+	return false if invalids.include? item
+	first_verifier = item[0...-2].split('').each.with_index.reduce(0) { |sum, (n, i)| sum + (n.to_i * (13 - (i > 3 ? i : i + 8))) } * 10 % 11
+	first_verifier = 0 if first_verifier == 10 || first_verifier == 11
+	return false if first_verifier != item[-2].to_i
+	second_verifier = item[0...-1].split('').each.with_index.reduce(0) { |sum, (n, i)| sum + (n.to_i * (14 - (i > 4 ? i : i + 8))) } * 10 % 11
+	second_verifier = 0 if second_verifier == 10 || second_verifier == 11
+	return false if second_verifier != item[-1].to_i
+	return true
+end
+
+class IsPhone
+	def self.===(item)
+		item =~ /^\+[1-9][0-9]\d{1,14}$/
+	end
+end
+
+class IsEmail
+	def self.===(item)
+		item =~ /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+	end
+end
+
+class IsEVP
+	def self.===(item)
+		item =~ /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/
 	end
 end
