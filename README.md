663c0e7564e09ae7df8cec0037452df45c8b1d70
Pedro H Domingues <pedroddomingues@gmail.com>
2022-07-10 12:12:43 -0300

    done with my best

--- README.md
+++ README.md
@@ -1,3 +1,55 @@
+# This is de documentation of my implementation of this challenge
+
+First of all, I've never used Ruby and that's the first time I do something with it. Ruby's syntax is pretty straight-forward so I've managed to solve the challenge in one day of studies, like some readings I read said: it's a conversational language; coding in Ruby feels like speaking to the computer. I was relutant to learn Ruby but now I really enjoy it.
+
+## My thoughts about the implementation
+
+- **TDD is awesome**: defining what to expect and coding based on this expectations are simply awesome! It grants code stability and prevents bugs. Also makes coding faster.
+- **OOP**: this is the first time I code using OOP, the way to think the code is a lot different than C (for example).
+- **Pix key types**: I used the types' regex to validate the keys but the provided URL is not well formatted to display the email regex so I've found a link to the [email standard](https://html.spec.whatwg.org/multipage/input.html#valid-e-mail-address) in a pdf version of DICT API docs.
+- **Exceptions**: based on the tests, a new instance of PixKey is created even if the key is invalid. Maybe there is something I don't know about OOP but my approach would be to raise exceptions when something is wrong.
+
+---
+
+When I was writing the line above I tough "well, maybe theses validations could be in a parent class and the PixKey inherit its methods and @type attribute after the validations" and I did refactor the code at least 3 times and I still wasn't satisfied.
+The "magic strings" as types were driving me crazy too. That's when I started to think about creating a constant defining every PixKey type. While I was writing the array of strings I thought maybe would be good to store the validations with the types... and then boom. Everything was good. After days refactoring the code over and over I was finally pleased with it, except the types methods. So after some research I implemented the methods dynamically.
+
+At this moment I think the code is almost ready to be delivered. Thinking about the validations: two classes were created - one to validate a phone and the other to validate cpf and cnpj. This classes are off of the PixKey class because they can evaluate any input from anywhere and could be used in any other scope. Some general validations could take place in the code too.......... well let's refactor the code one more time.
+For now it only checks if the new_key is a String. Following this structure, any new validation could be implemented, like checking if the new_key is already in use. (Maybe this kind of validation could be done to @key - between the steps 2 and 3 below)
+
+The final initialize flow is as follows:
+1. **Validate the input (new_key)** - checks if the input is a String
+   - If the input is invalid, the @key is an empty string and the object is invalid with @type 'error'
+2. **Set the object attribute @key to new_key.strip.freeze** - allows the input string to have blank spaces before and after the value
+3. **Runs the types validations** - it sets the @type with the first type match while running the validations (at this moment there aren't any possible string to match more than one type validation)
+   - If the @key is invalid, it stays as it is and the object is invalid with @type 'error'. This approach was meant so if it is invalid with a key, then the key didn't pass the validations but if the key is empty, the input was invalid.
+
+## What I have learned so far
+
+**I just can't believe how much I've learned with this project.** Since Ruby basics to OOP... here is a list:
+- Ruby basic syntax
+- Ruby scopes:
+  - Class
+  - Object
+  - Metaclass
+- OOP:
+  - How to abstract an idea to be implemented with OOP
+  - Differences in public, private and protected methods
+  - Inheritance
+- TDD
+- Document the code using comments
+  - I didn't like it at first but now I think it's good if they are not too much
+- Ruby lambdas and Procs differences and how to store then in a variable
+
+## That's all folks. I am really happy on how it ended.
+A big thanks to the Husky team for this project. I am really amazed by Ruby and will try to do some Rails project right now.
+
+
+## The original README is below
+---
+
+
+
 # Husky Academy
 
 Olá seja bem-vindo(a) ao nosso teste técnico.
--- pix_key/lib/pix_key.rb
+++ pix_key/lib/pix_key.rb
@@ -1,177 +1,221 @@
 # frozen_string_literal: true
 
-# This class defines a PixKey
-class PixKey
-  @@keys = []
+# The validators classes should be in another file 'validators.rb' but we were allowed to edit just this file.
+# This validations were not requested but I think they are relevant
 
-  attr_reader :key, :type, :valid
+# This class defines a CPFandCNPJValidator
+class CPFandCNPJValidator
+  # These two public methods are used to validate CPF and CNPJ
+  def valid_cpf?(item)
+    invalids = %w[12345678909 11111111111 22222222222 33333333333 44444444444 55555555555 66666666666 77777777777
+                  88888888888 99999999999 00000000000]
+    return false if invalids.include? item
 
-  # This method overrides the default new method to prevent creating multiple instances of the same key.
-  # Something like uniqueness validation from rails
-  def self.new(new_key)
-    instance = @@keys.select { |key| key.key?(new_key) }
-    unless instance.empty?
-      return instance[0][new_key]
-      # raise ArgumentError, "This key already has been created."
+    first_verifier = get_verifier(item, 'cpf', 1)
+    return false if first_verifier != item[-2].to_i
+
+    second_verifier = get_verifier(item, 'cpf', 2)
+    return false if second_verifier != item[-1].to_i
+
+    true
+  end
+
+  def valid_cnpj?(item)
+    invalids = %w[11111111111111 22222222222222 33333333333333 44444444444444 55555555555555 66666666666666
+                  77777777777777 88888888888888 99999999999999 00000000000000]
+    return false if invalids.include? item
+
+    first_verifier = get_verifier(item, 'cnpj', 1)
+    return false if first_verifier != item[-2].to_i
+
+    second_verifier = get_verifier(item, 'cnpj', 2)
+    return false if second_verifier != item[-1].to_i
+
+    true
+  end
+
+  private
+
+  def get_verifier(item, type, index)
+    item_arr = str_to_arr(item, index)
+    case type
+    when 'cpf'
+      res = cpf_reducer(item_arr, index)
+    when 'cnpj'
+      res = cnpj_reducer(item_arr, index)
     end
-
-    super(new_key)
+    [10, 11].include?(res) ? 0 : res
   end
 
-  def initialize(key)
-    if key.class != String
-      @key = ''
-      @type = 'error'
-      @valid = false
-      # raise TypeError, "Key must be a String."
-      return
-    end
-
-    @key = key.strip.freeze
-    @valid = true
-    @type = get_key_type(@key)
-
-    if @type == 'error'
-      @key = ''
-      @valid = false
-    end
-
-    @valid ? @@keys.push({ @key => self }) : nil
+  def str_to_arr(str, index)
+    str[0...(-3 + index)].split('')
   end
 
-  def valid?
-    @valid
+  def cpf_reducer(arr, index)
+    arr.each.with_index.reduce(0) { |sum, (n, i)| sum + (n.to_i * ((9 + index) - i)) } * 10 % 11
   end
 
-  def invalid?
-    !@valid
-  end
-
-  def value
-    @key
-  end
-
-  def phone?
-    @valid ? @type == 'phone' : false
-  end
-
-  def cpf?
-    @valid ? @type == 'cpf' : false
-  end
-
-  def cnpj?
-    @valid ? @type == 'cnpj' : false
-  end
-
-  def email?
-    @valid ? @type == 'email' : false
-  end
-
-  def evp?
-    @valid ? @type == 'evp' : false
-  end
-
-  def to_s
-    @key
-  end
-end
-
-#----------------- validations -----------------#
-
-def get_key_type(key)
-  case key
-  when IsCPF
-    'cpf'
-  when IsCNPJ
-    'cnpj'
-  when IsPhone
-    'phone'
-  when IsEmail
-    'email'
-  when IsEVP
-    'evp'
-  else
-    'error'
-    # raise ArgumentError, "Key is not a valid type of any kind."
-  end
-end
-
-# Validates if the given string is a CPF
-class IsCPF
-  def self.===(item)
-    item =~ /^[0-9]{11}$/ # ? valid_cpf?(item) : false
-    # This verification is commented because the default test uses an invalid CPF and fails
-  end
-end
-
-# Validates if the given string is a CNPJ
-class IsCNPJ
-  def self.===(item)
-    item =~ /^[0-9]{14}$/ ? valid_cnpj?(item) : false
-  end
-end
-
-# Validates if the given string is a valid CPF
-def valid_cpf?(item)
-  invalids = %w[12345678909 11111111111 22222222222 33333333333 44444444444 55555555555 66666666666 77777777777
-                88888888888 99999999999 00000000000]
-  return false if invalids.include? item
-
-  first_verifier = get_verifier(item, 'cpf', 1)
-  return false if first_verifier != item[-2].to_i
-
-  second_verifier = get_verifier(item, 'cpf', 2)
-  return false if second_verifier != item[-1].to_i
-
-  true
-end
-
-# Validates if the given string is a valid CNPJ
-def valid_cnpj?(item)
-  invalids = %w[11111111111111 22222222222222 33333333333333 44444444444444 55555555555555 66666666666666
-                77777777777777 88888888888888 99999999999999 00000000000000]
-  return false if invalids.include? item
-
-  first_verifier = get_verifier(item, 'cnpj', 1)
-  return false if first_verifier != item[-2].to_i
-
-  second_verifier = get_verifier(item, 'cnpj', 2)
-  return false if second_verifier != item[-1].to_i
-
-  true
-end
-
-def get_verifier(item, type, index)
-  case type
-  when 'cpf'
-    result = item[0...-(-3 + index)].split('').each.with_index.reduce(0) do |sum, (n, i)|
-      sum + (n.to_i * ((9 + index) - i))
-    end * 10 % 11
-  when 'cnpj'
-    result = item[0...(-3 + index)].split('').each.with_index.reduce(0) do |sum, (n, i)|
+  def cnpj_reducer(arr, index)
+    arr.each.with_index.reduce(0) do |sum, (n, i)|
       sum + (n.to_i * ((12 + index) - (i > (2 + index) ? i : i + 8)))
     end * 10 % 11
   end
-  [10, 11].include?(result) ? 0 : result
 end
 
-# Validates if the given string is a phone
-class IsPhone
-  def self.===(item)
-    item =~ /^\+[1-9][0-9]\d{1,14}$/
+# The PixKey phone type follows the E.164 recommendation from ITU-T, so the validator checks for the '+' sign,
+# valid DDI and valid DDD. BACEN allows a foreign phone number as PixKey, but some banks do not have it implemented
+# so I decided to accept just the brazilian DDI, if in the future we accept other DDIs just add them in the constant
+
+# This class defines a PhoneValidator
+class PhoneValidator
+  DDD_VALIDS = %w[11 12 13 14 15 16 17 18 19 21 22 24 27 28 31 32 33 34
+                  35 37 38 41 42 43 44 45 46 47 48 49 51 53 54 55 61 62 63 64
+                  65 66 67 68 69 71 73 74 75 77 79 81 82 83 84 85 86 87 88 89 91 92 93 94 95 96 97 98 99].freeze
+  DDI_VALIDS = ['55'].freeze
+
+  def valid_phone?(item)
+    return false unless plus_sign?(item)
+
+    return false unless valid_ddi?(item)
+
+    return false if item[1..2] == '55' && !valid_ddd?(item)
+
+    true
+  end
+
+  private
+
+  def plus_sign?(item)
+    item[0] == '+'
+  end
+
+  def valid_ddi?(item)
+    DDI_VALIDS.include?(item[1..2])
+  end
+
+  def valid_ddd?(item)
+    DDD_VALIDS.include?(item[3..4])
   end
 end
 
-# Validates if the given string is an email
-class IsEmail
-  def self.===(item)
-    item =~ %r{^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$}
-  end
-end
+# This class defines a PixKey
+class PixKey
+  # This constant defines the PixKeyTypes, add any new PixKeyType here so new methods and validations will be available
+  # FROM DICT API: "Novos tipos de chave poderão vir a ser adicionados no futuro. Logo, é importante que a
+  # implementação de clientes seja flexível, permitindo a adição de novos tipos de chave."
+  # A PixKeyType is a hash with a name as string and a validation as an array of validators being hash of type that can
+  # be 'regex' or 'lambda' and its value
+  PIX_KEY_TYPES = [
+    {
+      name: 'phone',
+      validations: [
+        { type: 'regex', value: /^\+[1-9][0-9]\d{1,14}$/ },
+        { type: 'lambda', value: ->(item) { PhoneValidator.new.valid_phone?(item) } }
+      ]
+    },
+    {
+      name: 'cpf',
+      validations: [
+        { type: 'regex', value: /^[0-9]{11}$/ }
+        # This validation is commented because the test uses an invalid cpf at lines 73 and 89
+        # { :type => 'lambda', :value => ->(item) { CPFandCNPJValidator.new.valid_cpf?(item) } }
+      ]
+    },
+    {
+      name: 'cnpj',
+      validations: [
+        { type: 'regex', value: /^[0-9]{14}$/ },
+        { type: 'lambda', value: ->(item) { CPFandCNPJValidator.new.valid_cnpj?(item) } }
+      ]
+    },
+    {
+      name: 'email',
+      validations: [
+        { type: 'regex', value: %r{^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]
+        {0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$}x }
+      ]
+    },
+    {
+      name: 'evp',
+      validations: [
+        { type: 'regex', value: /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/ }
+      ]
+    }
+  ].freeze
 
-# Validates if the given string is an EVP
-class IsEVP
-  def self.===(item)
-    item =~ /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/
+  NEW_KEY_VALIDATIONS = [
+    {
+      type: 'lambda',
+      value: ->(item) { item.instance_of?(String) }
+    }
+  ].freeze
+
+  attr_reader :key, :type
+
+  alias value key
+
+  def initialize(new_key)
+    new_key_validations(new_key)
+    return if @type == 'error'
+
+    @key = new_key.strip.freeze
+    types_validations
+  end
+
+  PIX_KEY_TYPES.each do |type|
+    define_method("#{type[:name]}?") { @type == type[:name] }
+  end
+
+  def valid?
+    @type != 'error'
+  end
+
+  def invalid?
+    @type == 'error'
+  end
+
+  def to_s = @key
+
+  def ==(other)
+    self.class == other.class &&
+      @key == other.key &&
+      @type == other.type &&
+      valid? == other.valid?
+  end
+
+  private
+
+  def new_key_validations(new_key)
+    NEW_KEY_VALIDATIONS.each do |validation|
+      validate_new_key(validation, new_key)
+      break if @type == 'error'
+    end
+  end
+
+  def validate_new_key(validation, new_key)
+    return if validation[:value].call(new_key)
+
+    @type = 'error'
+    @key = ''
+  end
+
+  def types_validations
+    PIX_KEY_TYPES.each do |type|
+      validate_type(type)
+      break if @type != 'error'
+    end
+  end
+
+  def validate_type(type)
+    type[:validations].each do |validation|
+      @type = type[:name]
+      case validation[:type]
+      when 'regex'
+        @type = 'error' unless validation[:value].match(@key)
+      when 'lambda'
+        @type = 'error' unless validation[:value].call(@key)
+      end
+      break if @type == 'error'
+    end
   end
 end
